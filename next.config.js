461c0bc00fbb2264eb15c1b7dbaa077ed6e5006c
Lyorrei Quintão <you@example.com>
2021-04-12 11:09:13 -0300

    Teste de resolução de imagem no shop

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const withImages = require('next-images')
 module.exports = withImages({
     esModule: true,
-    images: {
-        domains: ['localhost', 'https://dunna.herokuapp.com']
-    }
+    // images: {
+    //     domains: ['localhost', 'https://dunna.herokuapp.com']
+    // }
 })
 
 // module.exports = {
