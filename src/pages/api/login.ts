569386db8bafcc1354264c61aabb756398b3d03a
Lyorrei Quintão <you@example.com>
2021-04-16 11:25:58 -0300

    Tentativa de resolver problema de autenticação 2

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -14,7 +14,7 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
                     secure: process.env.NODE_ENV !== 'development',
                     maxAge: 2 * 60 * 60 * 1000,
                     path: '/',
-                    // sameSite: 'strict'
+                    sameSite: 'lax'
                         // process.env.NODE_ENV !== 'development' ? 'none' : 'lax'
                 })
             )
