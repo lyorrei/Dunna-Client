2f693378632084d1b1dfece591d59c5b3091df87
Lyorrei Quintão <you@example.com>
2021-04-26 20:17:03 -0300

    Tentativa de fixar site

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -15,44 +15,47 @@ interface Props {}
 
 const company: React.FC<Props> = props => {
     return (
-        <PageContainer
-            variants={pageContainerVariant}
-            initial="hidden"
-            animate="visible"
-        >
+        <>
             <Head>
                 <title>Dunna - Empresa</title>
             </Head>
-            <Company>
-                <Container>
-                    <Content>
-                        <TitleContainer>
-                            <h2>Sobre a empresa</h2>
-                        </TitleContainer>
-                        <TextContainer>
-                            <p>
-                                Lorem ipsum dolor, sit amet consectetur
-                                adipisicing elit. Possimus magni, autem aperiam,
-                                nobis quaerat quia culpa veritatis odit dolores
-                                iusto perspiciatis laboriosam assumenda
-                                asperiores earum ex voluptas corrupti, illum
-                                quibusdam Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Veniam, architecto. Nihil, ad
-                                nisi quos veniam illum tenetur ullam natus
-                                exercitationem ducimus nulla accusamus ipsam
-                                optio perferendis repellendus doloremque
-                                quisquam aut Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Repellat, eius
-                                sint blanditiis quae quasi illum accusantium ab
-                                doloremque doloribus et numquam hic ducimus
-                                consequatur nesciunt quaerat. Possimus aut
-                                commodi sapiente.
-                            </p>
-                        </TextContainer>
-                    </Content>
-                </Container>
-            </Company>
-        </PageContainer>
+            <PageContainer
+                variants={pageContainerVariant}
+                initial="hidden"
+                animate="visible"
+            >
+                <Company>
+                    <Container>
+                        <Content>
+                            <TitleContainer>
+                                <h2>Sobre a empresa</h2>
+                            </TitleContainer>
+                            <TextContainer>
+                                <p>
+                                    Lorem ipsum dolor, sit amet consectetur
+                                    adipisicing elit. Possimus magni, autem
+                                    aperiam, nobis quaerat quia culpa veritatis
+                                    odit dolores iusto perspiciatis laboriosam
+                                    assumenda asperiores earum ex voluptas
+                                    corrupti, illum quibusdam Lorem ipsum dolor
+                                    sit amet consectetur adipisicing elit.
+                                    Veniam, architecto. Nihil, ad nisi quos
+                                    veniam illum tenetur ullam natus
+                                    exercitationem ducimus nulla accusamus ipsam
+                                    optio perferendis repellendus doloremque
+                                    quisquam aut Lorem ipsum dolor sit amet
+                                    consectetur adipisicing elit. Repellat, eius
+                                    sint blanditiis quae quasi illum accusantium
+                                    ab doloremque doloribus et numquam hic
+                                    ducimus consequatur nesciunt quaerat.
+                                    Possimus aut commodi sapiente.
+                                </p>
+                            </TextContainer>
+                        </Content>
+                    </Container>
+                </Company>
+            </PageContainer>
+        </>
     )
 }
 
