d6390461bc8735c2b4f7de8c5887fd3c1fd007a3
lyorrei <reishono@outlook.com>
2021-11-03 19:32:26 -0300

    Adicionei otimização de imagem na página de shop 4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withImages = require('next-images')
 module.exports = withImages({
     esModule: true,
     inlineImageLimit: false,
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://dunna-upload.s3.amazonaws.com' : '',
     images: {
         domains: ['localhost', 'https://dunna.herokuapp.com', 'https://www.dunnajoalheria.com.br', 'dunna-upload.s3.amazonaws.com']
     }
